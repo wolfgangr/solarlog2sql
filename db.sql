11f4dc652f4f2c78edac3963f27baeda6da978c0
Wolfgang Rosner <wrosner@tirnet.de>
2020-12-11 00:11:12 +0100

    create normalized tables for minute data

--- db.sql
+++ db.sql
@@ -73,3 +73,44 @@ ALTER TABLE `months`
 ALTER TABLE `years`
   ADD PRIMARY KEY (`Date`,`Inv`);
 
+
+-- Adminer 4.7.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `min_INV`;
+CREATE TABLE `min_INV` (
+  `DateTime` datetime NOT NULL,
+  `Inv` int(3) NOT NULL,
+  `DaySum` int(10) DEFAULT NULL,
+  `Uac` int(5) DEFAULT NULL,
+  `Iac` int(5) DEFAULT NULL,
+  `Pac` int(8) DEFAULT NULL,
+  `Temp` int(3) DEFAULT NULL,
+  `Status` int(2) DEFAULT NULL,
+  `Error` int(2) DEFAULT NULL,
+  PRIMARY KEY (`DateTime`,`Inv`),
+  KEY `Inv` (`Inv`),
+  KEY `DateTime` (`DateTime`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='aus min*.csv, Inverterdaten';
+
+
+DROP TABLE IF EXISTS `min_MPP`;
+CREATE TABLE `min_MPP` (
+  `DateTime` datetime NOT NULL,
+  `Inv` int(3) NOT NULL,
+  `MPP` int(2) NOT NULL,
+  `Udc` int(5) DEFAULT NULL,
+  `Idc` int(5) DEFAULT NULL,
+  `Pdc` int(8) DEFAULT NULL,
+  PRIMARY KEY (`DateTime`,`Inv`,`MPP`),
+  KEY `Inv_MPP` (`Inv`,`MPP`),
+  KEY `DateTime` (`DateTime`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='aus min*.csv, string-Daten';
+
+
+-- 2020-12-10 22:24:16
+
