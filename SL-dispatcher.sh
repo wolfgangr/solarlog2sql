b7a054aea7d8e2fc6fb0ab920a7554d238598746
Wolfgang Rosner <wrosner@tirnet.de>
2020-12-11 15:43:34 +0100

    case switch boilerplate

--- SL-dispatcher.sh
+++ SL-dispatcher.sh
@@ -4,6 +4,46 @@
 # should work fine with icrontab
 # usage: SL-dispatcher.sh path file
 
-echo $2
-echo $1
+# echo "file" $2
+# echo "path" $1
 
+FILENAME=${2}
+PATHNAME=${1}
+FULLPATH=${PATHNAME}/${FILENAME}
+
+echo -e "\t...dispatching ${FULLPATH}"
+
+case ${FILENAME} in
+
+	events.js )
+		echo "events.js still to do"
+		;;
+
+	*.js )
+		echo "don't like to talk js"
+		;;
+
+	min??????.csv )
+		echo "call minute parser"
+		;;
+
+	days.csv )
+		echo "call day parser"
+		;;
+	
+	months.csv )
+		echo "call month parser"
+                ;;
+
+ 
+	years.csv )
+		echo "call year parser"
+                ;;
+
+	pm.csv )
+		echo "pm parser still to do"
+		;;
+
+	* )
+		echo " do nothing with ${FULLPATH}"
+esac
